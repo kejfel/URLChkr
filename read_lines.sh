9ccb0f53019832a61dd0b50911b249d7b5980f7a
Eric C. Weig <eweig@email.uky.edu>
2016-11-21 13:21:48 -0500

    Update read_lines.sh

--- read_lines.sh
+++ read_lines.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# @copyright Copyright 2016 Eric C. Weig 
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Checking URL from file: $line"
     sh ./check_urls.sh "$line"
